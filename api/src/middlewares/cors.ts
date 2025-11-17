503a931f238a72631b773dc9e3f20b9adfb3e13a
alexei <alexeialvarez4@gmail.com>
2025-11-16 19:32:36 -0600

    add domain to cors fix

--- api/src/middlewares/cors.ts
+++ api/src/middlewares/cors.ts
@@ -5,7 +5,7 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:1234',
   'http://127.0.0.1:5500',
   'http://localhost:4200',
-  'https://vetcareapi-production.up.railway.app',
+  'https://vetcare-production.up.railway.app',
 ];
 
 export const corsMiddleWare = ({ acceptedOrigin = ACCEPTED_ORIGINS } = {}) =>
